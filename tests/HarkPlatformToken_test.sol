601645be89a971f777b69a36d981345d7911b3ac
DESKTOP-8DRVUQ7\Jerem <me4jeremyboett@gmail.com>
2021-05-13 17:10:02 -0700

    Added extra tests

--- tests/HarkPlatformToken_test.sol
+++ tests/HarkPlatformToken_test.sol
@@ -17,7 +17,7 @@ contract testSuite {
         // Here should instantiate tested contract
         platform = new HarkPlatformToken(100);
         
-        Assert.equal(uint(platform.tax()), uint(100), "Tax isn't set properly.");
+        Assert.equal(uint(platform.tax()), uint(100), "Tax isn't set properly upon construction.");
         Assert.equal(bool(true), bool(platform.balanceOf(address(this)) > 0), "No initial mint given to the creator.");
     }
 
@@ -25,29 +25,31 @@ contract testSuite {
         platform.setTax(150);
         Assert.equal(uint(150), uint(platform.tax()), "Tax rate should be 150 after setting it to 150.");
         
-        platform.setTax(30000);
-        Assert.equal(uint(150), uint(platform.tax()), "Tax rate should still be 150 after setting it to 150.");
-        
         platform.setTax(100);
         Assert.equal(uint(100), uint(platform.tax()), "Tax rate should be 100 after setting it to 100.");
     }
-
-    function checkSuccess2() public pure returns (bool) {
-        // Use the return value (true or false) to test the contract
-        return true;
-    }
     
-    function checkFailure() public {
-        Assert.equal(uint(1), uint(2), "1 is not equal to 2");
+    function checkSetMinimumGovernanceTokenRequirement() public {
+        platform.setMinimumSupplyForNomination(1000);
+        Assert.equal(uint(1000), uint(platform.minimumSupplyForNomination()), "MinimumSupplyForNomination should be 1000 after setting it to 1000.");
+        
+        platform.setMinimumSupplyForNomination(5000);
+        Assert.equal(uint(5000), uint(platform.minimumSupplyForNomination()), "MinimumSupplyForNomination should be 5000 after setting it to 5000.");
     }
 
-    /// Custom Transaction Context
-    /// See more: https://remix-ide.readthedocs.io/en/latest/unittesting.html#customization
-    /// #sender: account-1
-    /// #value: 100
-    function checkSenderAndValue() public payable {
-        // account index varies 0-9, value is in wei
-        Assert.equal(msg.sender, TestsAccounts.getAccount(1), "Invalid sender");
-        Assert.equal(msg.value, 100, "Invalid value");
+    function checkCreatingElection() public {
+        Assert.equal(platform.getElectionCount(), 0, "Election count should be 0 when creating.");
+
+        // created the first election        
+        uint ending = block.timestamp + 50;
+        uint newVoteElection = platform.createVote(ending);
+
+        // checks that everything about the first election was right
+        Assert.equal(newVoteElection, 0, "First election id was not 0.");
+        Assert.equal(platform.getElectionCount(), 1, "Election count was not 1 after adding the first stream.");
+        Assert.equal(false, platform.electionHasEnded(newVoteElection), "New election should not have ended.");
+        Assert.equal(0, platform.getNomineeCount(0), "New election should not have any nominees before adding them.");
+        
+        // 
     }
 }
